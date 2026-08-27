ebd09a57452360910066d5a0f9226de8989a1941
m2agroup2026-crypto <m2agroup2026@gmail.com>
2026-08-27 12:10:57 +0300

    perf: remove duplicate remote font loading

--- app/(ar)/layout.tsx
+++ app/(ar)/layout.tsx
@@ -11,18 +11,6 @@ export default function ArabicRootLayout({
 }>) {
   return (
     <html lang="ar" dir="rtl">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;500;600;700;800&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body>{children}</body>
     </html>
   );
