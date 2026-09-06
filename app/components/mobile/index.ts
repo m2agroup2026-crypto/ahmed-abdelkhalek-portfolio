1f50839b35c819d596711f8749688c98ed75825f
m2agroup2026-crypto <m2agroup2026@gmail.com>
2026-09-06 23:07:49 +0300

    Create mobile experience exports

---
+++ app/components/mobile/index.ts
@@ -0,0 +1,7 @@
+export { default as MobileHeader } from './MobileHeader';
+export { default as MobileHero } from './MobileHero';
+export { default as MobileCaseStudies } from './MobileCaseStudies';
+export { default as MobileSystems } from './MobileSystems';
+export { default as MobileJourney } from './MobileJourney';
+export { default as MobileFooter } from './MobileFooter';
+export { default as MobileContact } from './MobileContact';
