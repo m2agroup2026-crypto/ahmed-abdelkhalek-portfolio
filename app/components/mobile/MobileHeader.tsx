e258f7fae43576cfee56cb9e5bb16a63bd050f63
m2agroup2026-crypto <m2agroup2026@gmail.com>
2026-09-06 23:05:44 +0300

    Create mobile experience foundation header

---
+++ app/components/mobile/MobileHeader.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function MobileHeader() {
+  return (
+    <header className="mobile-header-v2">
+      <div className="mobile-brand">AA</div>
+      <button className="mobile-menu-button" aria-label="Open menu">
+        <span />
+        <span />
+      </button>
+    </header>
+  );
+}
