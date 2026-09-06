bed60d7e0ebc65c6f7db7c8bb72a5a13a5af9d91
m2agroup2026-crypto <m2agroup2026@gmail.com>
2026-09-06 23:08:20 +0300

    Add mobile experience composition layer

---
+++ app/components/mobile/MobileExperience.tsx
@@ -0,0 +1,19 @@
+import MobileHero from './MobileHero'
+import MobileSystems from './MobileSystems'
+import MobileCaseStudies from './MobileCaseStudies'
+import MobileJourney from './MobileJourney'
+import MobileContact from './MobileContact'
+import MobileFooter from './MobileFooter'
+
+export default function MobileExperience() {
+  return (
+    <main className="mobile-experience-v2">
+      <MobileHero />
+      <MobileSystems />
+      <MobileCaseStudies />
+      <MobileJourney />
+      <MobileContact />
+      <MobileFooter />
+    </main>
+  )
+}
